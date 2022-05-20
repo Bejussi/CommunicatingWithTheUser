1a3f996d6a959168be25d593c8eb2e47372c59a7
Alexandra <pindooqy@gmail.com>
2022-05-20 12:24:37 +0300

    Create basic notification

--- app/src/main/java/com/bejussi/communicatingwiththeuser/MainActivity.kt
+++ app/src/main/java/com/bejussi/communicatingwiththeuser/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.bejussi.communicatingwiththeuser
 
 import android.app.DatePickerDialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
@@ -18,6 +25,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.custom_toast_layout.*
 import kotlinx.android.synthetic.main.custom_toast_layout.view.*
 import java.util.*
+import androidx.core.app.NotificationCompat
+import usercomms.joemarini.example.com.usercommunications.NotificationResultActivity
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +41,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         dateDialogButton.setOnClickListener(this)
         choiseDialogButton.setOnClickListener(this)
         customDialogButton.setOnClickListener(this)
+        simpleNotificationButton.setOnClickListener(this)
+
+        // For API 26 and later, we have to create a channel otherwise the notification
+        // won't be displayed. This can be called multiple times without harm - if there's
+        // already a channel with the given ID then the call is ignored
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(NOTIFY_CHANNEL, "Notifications", importance)
+            channel.description = "This is a notification channel"
+
+            // Register the channel with the system
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(channel)
+        }
     }
 
     override fun onClick(p0: View?) {
@@ -44,7 +67,57 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.dateDialogButton -> showDateDialog()
             R.id.choiseDialogButton -> showChoiseDialog()
             R.id.customDialogButton -> showCustomDialog()
+            R.id.simpleNotificationButton -> showNotification()
         }
+    }
+
+    private fun showNotification() {
+        // TODO: create the NotificationCompat Builder
+        val builder = NotificationCompat.Builder(this, NOTIFY_CHANNEL)
+
+        // TODO: Create the intent that will start the ResultActivity when the user
+        // taps the notification or chooses an action button
+        val intent = Intent(this, NotificationResultActivity::class.java)
+
+        // TODO: Store the notification ID so we can cancel it later in the ResultActivity
+        intent.putExtra("notifyID", NOTIFY_ID)
+
+        // TODO: create a PendingIntent to fire for the notification
+        val pendingIntent = PendingIntent.getActivity(this, NOTIFY_ID, intent,
+            PendingIntent.FLAG_IMMUTABLE)
+
+        // TODO: Set the three required items all notifications must have
+        builder.setSmallIcon(R.drawable.ic_stat_sample_notification)
+        builder.setContentTitle("Sample Notification")
+        builder.setContentText("This is a sample notification")
+
+        // TODO: Set the notification to cancel when the user taps on it
+        builder.setAutoCancel(true)
+
+        // TODO: Set the large icon to be our app's launcher icon
+        builder.setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+
+        // TODO: Set the small subtext message
+        builder.setSubText("Tap to view")
+
+        // TODO: Set the content intent to launch our result activity
+        builder.setContentIntent(pendingIntent)
+
+        // TODO: Add an expanded layout to the notification
+
+
+        // TODO: Add action buttons to the Notification if they are supported
+        // Use the same PendingIntent as we use for the main notification action
+
+
+        // TODO: Set the lock screen visibility of the notification
+
+
+        // TODO: Build the finished notification and then display it to the user
+        val notification = builder.build()
+        val mgr = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        mgr.notify(NOTIFY_ID, notification)
+
     }
 
     private fun showSimpleDialog() {
@@ -97,5 +170,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun showStandartToast() {
         Toast.makeText(this,R.string.standard_toast_message, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private val NOTIFY_ID = 1001
+        private val NOTIFY_CHANNEL = "MY_CHANNEL"
     }
 }
---
+++ app/src/main/java/com/bejussi/communicatingwiththeuser/NotificationResultActivity.kt
@@ -0,0 +1,24 @@
+package usercomms.joemarini.example.com.usercommunications
+
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import com.bejussi.communicatingwiththeuser.R
+
+class NotificationResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification_result)
+
+        // When launched from an addAction Intent, we must manually cancel
+        // the notification otherwise it will stay in the status bar
+        val i = intent
+        val notifyID = i.getIntExtra("notifyID", 0)
+
+        val mgr = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        mgr.cancel(notifyID)
+    }
+}
